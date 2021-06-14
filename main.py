7584f16cb71900f4e7da5cda124a506fd9d31f7c
Marios1989 <68182435+Marios1989@users.noreply.github.com>
2021-06-14 16:06:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+import sys
+
+img = cv.imread(cv.samples.findFile('/home/user/Downloads/Santorini.jpg'))
+
+if img is None:
+    sys.exit("Could not read the image.")
+
+cv.imshow("Display window", img)
+k = cv.waitKey(0)
+
+if k == ord("s"):
+    cv.imwrite("Santorini.jpg", img)
